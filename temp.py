fa55f140897805a5e6be9da9f09753af72ddc4de
nonbiri3 <57429849+nonbiri3@users.noreply.github.com>
2020-10-10 19:23:18 +0900

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,23 @@
+import ambient
+ambi = ambient.Ambient(チャネルID,"ライトキー")
+
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+instance = dht11.DHT11(pin=4)
+
+while True:
+    result = instance.read()
+    if result.is_valid():
+        print("Last valid input: " + str(datetime.datetime.now()))
+        print("Temperature: %d C" % result.temperature)
+        print("Humidity: %d %%" % result.humidity)
+    r = ambi.send({"d1":result.temperature, "d2":result.humidity})
+    time.sleep(10)
